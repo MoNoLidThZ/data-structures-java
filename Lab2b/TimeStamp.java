c91d73e6a9df1ba5e838f2d6d066817ed7c4b4ce
ken1286 <ken.ridenour4@gmail.com>
2020-09-06 11:24:18 -0700

    added equals to lab2b

--- Lab2b/TimeStamp.java
+++ Lab2b/TimeStamp.java
@@ -1,11 +1,19 @@
+import java.util.Calendar;
+
 public class TimeStamp extends SimpleDate {
   private GlobalTime time;
   private String months[] = { "Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec" };
+  private static Calendar cal = Calendar.getInstance();
 
   public TimeStamp() {
-    super(1, 1, 1);
-    time = new GlobalTime();
+    super(cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), cal.get(Calendar.YEAR));
+    time = new GlobalTime(cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), cal.get(Calendar.SECOND),
+        cal.get(Calendar.ZONE_OFFSET));
   }
+  // public TimeStamp() {
+  // super(1, 1, 1);
+  // time = new GlobalTime();
+  // }
 
   public TimeStamp(int y) {
     super(1, 1, y);
@@ -46,6 +54,19 @@ public class TimeStamp extends SimpleDate {
     this(otherDate.getMonth(), otherDate.getDay(), otherDate.getYear());
   }
 
+  public boolean equals(Object o) {
+
+    if (o instanceof TimeStamp) {
+      return (((TimeStamp) o).getMonth() == this.getMonth() && ((TimeStamp) o).getDay() == this.getDay()
+          && ((TimeStamp) o).getYear() == this.getYear() && this.time.getHour() == ((TimeStamp) o).time.getHour()
+          && this.time.getMinute() == ((TimeStamp) o).time.getMinute()
+          && this.time.getSecond() == ((TimeStamp) o).time.getSecond()
+          && this.time.getZone() == ((TimeStamp) o).time.getZone());
+    } else {
+      return false;
+    }
+  }
+
   public void changeZone(int zone) {
     if (zone >= -12 || zone <= 12) {
       int difference = zone - time.getZone();
@@ -77,17 +98,34 @@ public class TimeStamp extends SimpleDate {
   }
 
   public static void main(String[] args) {
+
     SimpleDate sd = new SimpleDate(10, 15, 2001);
     TimeStamp ts = new TimeStamp(12, 29, 1986, 10, 50, 45, -8);
     TimeStamp ts2 = new TimeStamp(sd);
 
+    // Testing output
+    System.out.println(sd); // SimpleDate
     System.out.println(ts); // Output: Dec 29, 2009 at 10:50:45 UTC-2
-    System.out.println(ts2);
+    System.out.println(ts2); // Testing SimpleDate copy to TimeStamp
+
+    System.out.println();
 
     // testing changeZone method
-    TimeStamp cz = new TimeStamp(23, 11, 1990, 10, 50, 45, 2);
-    System.out.println(cz);
-    cz.changeZone(-2);
-    System.out.println(cz);
+    TimeStamp cz = new TimeStamp(23, 11, 1990, 10, 50, 45, 6);
+    System.out.println("Original zone: " + cz);
+    cz.changeZone(3);
+    System.out.println("Changed zone: " + cz);
+
+    System.out.println();
+
+    // Default constructor is based on the current system time
+    TimeStamp curTime = new TimeStamp();
+    System.out.println("Current time: " + curTime);
+
+    // Testing equals
+    TimeStamp ts3 = new TimeStamp(12, 29, 1986, 10, 50, 45, -8);
+    TimeStamp ts4 = new TimeStamp(12, 29, 1986, 10, 50, 45, 6);
+    System.out.println(ts.equals(ts3)); // expecting true
+    System.out.println(ts3.equals(ts4)); // expecting false
   }
 }
